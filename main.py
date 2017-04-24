506a5406bd5e03d5a70d29f5b193f7c707971f53
Caspar89 <ccavandeursen@gmail.com>
2017-04-24 18:14:19 +0100

    setting up the main window

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import tkinter as Tk
 
+# The main AIGen class
 class AIGen (object):
 
+# TKinter window
+
+    # Initializes AIGen
     def __init__(self, parent):
         self.root = parent
         self.root.title("AIGen")
@@ -11,9 +15,31 @@ class AIGen (object):
         closeBtn = Tk.Button(self.frame, text="Close", command=self.shutdown)
         closeBtn.pack()
 
+    # Creates the console
+    def console(self):
+        console = tk.Text()
+        console.pack
+
+    # Shuts down AIGen
     def shutdown(self):
         self.root.destroy()
-        
+
+# Environment
+class Environment (object):
+
+    # Initializes the environment
+    def init_environment(self):
+        global environment = {}
+
+# Brain
+class Brain (object):
+
+    # Initializes the Brain
+    def init_brain(self):
+        global brain = {}
+
+
+# Starts AIGen
 if __name__ == "__main__":
     root=Tk.Tk()
     root.geometry("800x600")
